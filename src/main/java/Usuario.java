fde9148781b8878267c449e7084bd85ad7abbea7
leandro08rezende@gmail.com <leandro08rezende@gmail.com>
2020-02-08 11:21:38 -0300

    Initial Commit

---
+++ src/main/java/Usuario.java
@@ -0,0 +1,35 @@
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class Usuario {
+    private String usuario = "";
+    private String senha = "";
+    
+    public String logar(){
+        if(usuario.equals("Julio") && senha.equals("Julio")){
+            return "pag-sucesso";
+        }
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário inválido", "Usuário inválido");
+        ctx.addMessage(null, msg);
+        return "";              
+    }
+    
+    
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
